5087d3c47a5fddc1df9a88f090f439d122a41ca3
sport1028 <sport1028kr@naver.com>
2021-09-01 21:36:28 +0900

    message commit

--- src/server.js
+++ src/server.js
@@ -17,15 +17,18 @@ const server = http.createServer(app);
 
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
+  sockets.push(socket);
   console.log("Connected to Browser!! ✅");
   socket.on("close", () => {
     console.log("Disconnected from the Browser!! ❌");
   });
   socket.on("message", (message) => {
-    console.log(message.toString("utf8"));
+    sockets.forEach(aSocket => aSocket.send(message.toString("utf8")));
   });
-  socket.send("hello!!");
 });
 
 server.listen(3000, handleListen);
+

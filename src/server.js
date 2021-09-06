a4d2f1cb45b607a8a10b22bedc450806813c5863
sport1028 <sport1028kr@naver.com>
2021-09-07 08:47:24 +0900

    created socketIO module commit

--- src/server.js
+++ src/server.js
@@ -20,6 +20,12 @@ const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", socket => {
   console.log(socket);
+  socket.on("enter_room", (msg, done) => {
+    console.log(msg);
+    setTimeout(() => {
+      done();
+    }, 10000);
+  });
 });
 
 // const sockets = [];

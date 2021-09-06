9d74b1dd57d1bda0ace52c1f5d6d35a198df38e9
sport1028 <sport1028kr@naver.com>
2021-09-06 21:58:24 +0900

    installed socketIO commit

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,43 +1,45 @@
-const messageList = document.querySelector("ul");
-const nickForm = document.querySelector("#nick");
-const messageForm = document.querySelector("#message");
-const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = io(); 
 
-function makeMessage(type, payload) {
-  const msg = {type, payload};
-  return JSON.stringify(msg);
-}
+// const messageList = document.querySelector("ul");
+// const nickForm = document.querySelector("#nick");
+// const messageForm = document.querySelector("#message");
+// const socket = new WebSocket(`ws://${window.location.host}`);
 
-socket.addEventListener("open", () => {
-  console.log("Connected to Server!! ✅");
-});
+// function makeMessage(type, payload) {
+//   const msg = {type, payload};
+//   return JSON.stringify(msg);
+// }
 
-socket.addEventListener("message", (message) => {
-  const li = document.createElement("li");
-  li.innerText = message.data;
-  messageList.append(li);
-});
+// socket.addEventListener("open", () => {
+//   console.log("Connected to Server!! ✅");
+// });
 
-socket.addEventListener("close", () => {
-  console.log("Disconnected from Server!! ❌");
-});
+// socket.addEventListener("message", (message) => {
+//   const li = document.createElement("li");
+//   li.innerText = message.data;
+//   messageList.append(li);
+// });
 
-function handleSubmit(event) {
-  event.preventDefault();
-  const input = messageForm.querySelector("input");
-  socket.send(makeMessage("new_message", input.value));
-  const li = document.createElement("li");
-  li.innerText = `You : ${input.value}`;
-  messageList.append(li);
-  input.value = "";
-}
+// socket.addEventListener("close", () => {
+//   console.log("Disconnected from Server!! ❌");
+// });
 
-function handleNickSubmit(event) {
-  event.preventDefault();
-  const input = nickForm.querySelector("input");
-  socket.send(makeMessage("nickname", input.value));
-}
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const input = messageForm.querySelector("input");
+//   socket.send(makeMessage("new_message", input.value));
+//   const li = document.createElement("li");
+//   li.innerText = `You : ${input.value}`;
+//   messageList.append(li);
+//   input.value = "";
+// }
 
-messageForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
+// function handleNickSubmit(event) {
+//   event.preventDefault();
+//   const input = nickForm.querySelector("input");
+//   socket.send(makeMessage("nickname", input.value));
+// }
+
+// messageForm.addEventListener("submit", handleSubmit);
+// nickForm.addEventListener("submit", handleNickSubmit);
 
